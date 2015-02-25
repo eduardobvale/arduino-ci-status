55fafd362e4d27e2a73a61a27ed712c8d8240958
Christian Fei <crifei93@gmail.com>
2015-02-25 22:29:19 +0100

    clean up

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 var five = require('johnny-five')
   , board = new five.Board()
-  , net = require('net')
 
-var led = {}
-var buffer = ''
+var net = require('net')
 
 
-
-var client = connectToServer()
-
-function connectToServer(){
-  return net.connect({
-    host:'146.185.167.197',
-    port: 3001
-  }, function() {
-    console.log('connected to server!')
-  })
+var leds = {
+  green: null,
+  red:   null,
 }
 
-client
+
+var WEBHOOK_PUBLISHER_HOST = process.env.WEBHOOK_PUBLISHER_HOST || '146.185.167.197'
+var WEBHOOK_PUBLISHER_TCP_PORT = process.env.WEBHOOK_PUBLISHER_TCP_PORT || 3001
+
+var client = new net.Socket()
+client.connect(WEBHOOK_PUBLISHER_TCP_PORT, WEBHOOK_PUBLISHER_HOST, function() {
+  console.log('-- connected to server!')
+})
+.on('close', function(hadError) {
+  if (hadError)
+    console.log( '-- connection closed with error' )
+  else
+    console.log( '-- connection closed' )
+})
+.on('error', function(error) {
+  console.log( '-- error', error )
+})
 .on('data', function(binary) {
   var data = binary.toString()
-  buffer += data
-  console.log( 'stream -- got data' )
-})
-.on('end', function(){
-  console.log( 'stream -- end data' )
-  var jsonData
+  console.log( '-- data', data )
   try{
-    jsonData = JSON.parse(buffer)
+    jsonData = JSON.parse(data)
   } catch(e){}
 
-  data = ''
   if( !jsonData ){
-    console.log( '-- failed to parse json', buffer )
-    data = ''
+    console.log( '-- failed to parse json', data )
     return
   }
 
-  led.green.off()
-  led.red.off()
+  turnOnOff(leds.green,false)
+  turnOnOff(leds.red,false)
 
   if( jsonData.payload ){
-    if( /success/.test(jsonData.payload.status) )
-      led.green.on()
-    else
-      led.red.on()
+    if( /success/.test(jsonData.payload.status) ){
+      turnOnOff(leds.green,true)
+    }else{
+      turnOnOff(leds.red,true)
+    }
   } else {
     console.log( '-- json has no property payload' )
   }
-
-  client = connectToServer()
 })
 
 
 board.on('ready', function() {
-  led.green = new five.Led(3)
-  led.red = new five.Led(6)
+  leds.green = new five.Led(3)
+  leds.red = new five.Led(6)
 })
+
+
+
+function turnOnOff(led,turnOn){
+  if( led ){
+    turnOn ? led.on() : led.off()
+    console.log( 'led turned ' + (turnOn ? 'on' : 'off') )
+  }else{
+    console.log( 'no led to turn ' + (turnOn ? 'on' : 'off') )
+  }
+}
