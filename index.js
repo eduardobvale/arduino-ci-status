46f0391d5510225e56ecd7f2912beb159a1ddfa4
Christian Fei <crifei93@gmail.com>
2015-02-25 19:35:17 +0100

    fix

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 var five = require('johnny-five')
   , board = new five.Board()
+  , net = require('net')
 
 var led = {}
+var buffer = ''
+
+
+
+var client = connectToServer()
+
+function connectToServer(){
+  return net.connect({
+    host:'146.185.167.197',
+    port: 3001
+  }, function() {
+    console.log('connected to server!')
+  })
+}
+
+client
+.on('data', function(binary) {
+  var data = binary.toString()
+  buffer += data
+  console.log( 'stream -- got data' )
+})
+.on('end', function(){
+  console.log( 'stream -- end data' )
+  var jsonData
+  try{
+    jsonData = JSON.parse(buffer)
+  } catch(e){}
+
+  data = ''
+  if( !jsonData ){
+    console.log( '-- failed to parse json', buffer )
+    data = ''
+    return
+  }
+
+  led.green.off()
+  led.red.off()
+
+  if( jsonData.payload ){
+    if( /success/.test(jsonData.payload.status) )
+      led.green.on()
+    else
+      led.red.on()
+  } else {
+    console.log( '-- json has no property payload' )
+  }
+
+  client = connectToServer()
+})
+
 
 board.on('ready', function() {
   led.green = new five.Led(3)
   led.red = new five.Led(6)
-
-  led.green.fadeIn()
-  led.red.fadeIn()
 })
